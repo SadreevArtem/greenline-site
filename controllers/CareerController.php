c84dbf6dba58cd02fb4fc7ef34251e03a0b952d6
123 <a123@123s-MacBook-Air.local>
2025-02-27 12:09:48 +0500

    vacancies

--- controllers/CareerController.php
+++ controllers/CareerController.php
@@ -9,7 +9,7 @@ class CareerController extends FrontendController
 {
     public function actionIndex($id)
     {
-        if (!in_array($id, ['160420', '160410',])) {
+        if (!in_array($id, ['160420', '160410', '160430', '160440', '160450', '160460', '160470', '160480', '160490'])) {
             throw new NotFoundHttpException(\Yii::t('main', 'Страница не найдена'));
         }
         return $this->render('index', ['id' => $id]);
